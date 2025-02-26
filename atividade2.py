3802f1e42a043bc37d2c3330bcd6b2a43c4c0be1
Grazie <alvesgraziella9@gmail.com>
2025-02-26 20:43:03 -0300

    teste

---
+++ atividade2.py
@@ -0,0 +1,40 @@
+def  consultar():
+    busca= input ('''
+    ..........................................................
+       Lista de Alunos 3 ano Desenvolvimento de Sistemas
+     
+        1 - Cadastar Aluno
+        2 - Lista de nome 
+        4 - Notas 
+    ..........................................................
+        Informe a informação a ser acessada: 
+    ''')
+
+    if busca ==1:
+        cadastarAluno()
+    elif busca ==2:
+        mostarLista()
+    elif busca ==3:
+        notasAlunos()
+
+def cadastarAluno():
+    numeroChamada = input("Qual é o numero da chamada do Aluno (a): ")
+    nome = input ("Informe O nome do Aluno (a): ")
+
+
+
+
+def mostarLista():
+
+
+
+
+def notasAlunos():
+
+
+       
+
+
+
+def main():
+    consultar()
